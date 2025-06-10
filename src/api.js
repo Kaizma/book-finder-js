5cdc19354597ede122e5aa2781db0ad0db0c7e61
Kasun Hettiarachchi <kasunh1411@gmail.com>
2025-06-10 12:52:15 +0530

    Get all books request added

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export function getAllBooks(){
+    return fetch('https://www.googleapis.com/books/v1/volumes?q=javascript')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            console.log('Response status:', response.status);
+            console.log(response.json);
+            return response.json();
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+getAllBooks()
